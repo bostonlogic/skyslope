97f8bd1569f649e39225772cd14dbef40d3edffb
Mwaki Harri Magotswi <magotswi@gmail.com>
2021-08-10 18:06:07 -0400

    finally got a handle on the encoding issue :relieved:

--- lib/skyslope/resources/transaction_resource.rb
+++ lib/skyslope/resources/transaction_resource.rb
@@ -8,21 +8,28 @@ module Skyslope
         verb :get
         query_keys :modifiedAfter, :modifiedBefore, :createdAfter, :createdBefore, :closedAfter, :closedBefore, :status, :type, :email, :propertyAddress
         path 'api/files'
-        handler(200) { |response| TransactionMapping.extract_collection(response.body, :read) }
+        handler(200) do |response|
+          transactions_payload = response.body.gsub("\u0001", "\n").gsub("\u0000", "").gsub("\u001A",' ').gsub(/[^\x00-\x7F]/,'')
+          TransactionMapping.extract_collection(transactions_payload, :read)
+        end
       end
 
       action :listings do
         verb :get
         query_keys :agentGuid, :createdByGuid, :earliestDate, :latestDate, :pageNumber
         path 'api/files/listings'
-        handler(200) { |response| TransactionMapping.extract_collection(response.body, :read) }
+        handler(200) { |response|
+          TransactionMapping.extract_collection(response.body, :read)
+        }
       end
 
       action :sales do
         verb :get
         query_keys :agentGuid, :createdByGuid, :earliestDate, :latestDate, :pageNumber
         path 'api/files/sales'
-        handler(200) { |response| TransactionMapping.extract_collection(response.body, :read) }
+        handler(200) { |response|
+          TransactionMapping.extract_collection(response.body, :read)
+        }
       end
 
       # action :create_listing do
